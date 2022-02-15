d54da7e6a812f9373b79f8c1c881f2c8d7abe18f
Michael Roussell <55032397+Mroussell@users.noreply.github.com>
2022-02-15 12:07:40 -0500

    finished inital db handler class and post creating script to auto add post to the db

--- dbhandler.py
+++ dbhandler.py
@@ -13,6 +13,7 @@ MIT Education License Preferred.
 import os
 import psycopg2
 import psycopg2.extras
+from psycopg2 import sql
 import json
 from dotenv import load_dotenv
 
@@ -46,7 +47,7 @@ class DBHandler:
 		return cur
 
 
-	def create_table(self):
+	def create_table(self, table_name):
 		"""
 		Used the psycopg2 init functions to create table if one does not already exist. If it does throw error and pass.
 
@@ -54,13 +55,15 @@ class DBHandler:
 		connect = self.initdbconnect()
 		cursor = self.initdbcursor(connect)
 		try:
-			cursor.execute('''CREATE TABLE IF NOT EXISTS tech_projects_test (
+			query = '''CREATE TABLE IF NOT EXISTS ''' + table_name + ''' (
 			    POST_ID SERIAL NOT NULL,
 			    POST_IMG_LOC text,
+			    REPO_LINK text,
 			    TITLE varchar(100),
 			    BODY text,
 			    PRIMARY KEY (POST_ID)
-				);''')
+				);'''
+			cursor.execute(query)
 			connect.commit()
 			connect.close()
 			print(f"Table Created!")
@@ -106,25 +109,29 @@ class DBHandler:
 			pass		
 
 
-	def add_post(self, title:str, body:str, img_loc:str):
+	def add_post(self, table_name:str, title:str, body:str, img_loc:str, repo_link:str):
 		"""
 		Used the psycopg2 init functions to add a post to a table. If throws error, pass.
 
 		Parameters
 		----------
+			table_name : string (required)
+				A String object that determins the table location for the post to be added. Has no size limit.
 			title : string (required)
 				A String object that holds the title of the post, under 100 characters.
 			body : string (required)
 				A String object that hold the body of the post. This is converted to text dataype in SQL and has no size limit.
 			img_loc : string (required)
 				A String object that hold the image location for the post thumbnail. Has not size limit
+			repo_link: string (required)
+				A String object that hold the link to the github repo for the post. Has no size limit.
 		"""
 		connect = self.initdbconnect()
 		cursor = self.initdbcursor(connect)
 		try:
-			cursor.execute('''INSERT INTO tech_projects_test (TITLE, BODY, POST_IMG_LOC)
-				VALUES (%s, %s, %s);''',
-				(title, body, img_loc)
+			cursor.execute(sql.SQL('''INSERT INTO {} (TITLE, BODY, POST_IMG_LOC, REPO_LINK)
+				VALUES (%s, %s, %s, %s);''').format(sql.Identifier(table_name)),
+				(title, body, img_loc, repo_link)
 			)
 			connect.commit()
 			connect.close()
@@ -167,12 +174,41 @@ class DBHandler:
 			pass
 
 
-dbh = DBHandler()
-table_name = 'tech_projects_test'
+	def clear_table_rows(self, table_name:str):
+		"""
+		Use the psycopg2 init function to delete a post from table. If throws error, pass.
+
+		Parameters
+		----------
+			post_id : int (required)
+				An integer object that represents the post id column identifier to be deleted.
+			table_name: string (required)
+				A String object that indentifies the table for which row should be deleted.
+		"""	
+		connect = self.initdbconnect()
+		cursor = self.initdbcursor(connect)
+		try:
+			query = "DELETE FROM " + table_name + ";"
+			cursor.execute(query)
+			connect.commit()
+			connect.close()
+			print(f"Table {table_name} cleared of ALL rows.")
+		except ValueError:
+			print(f"Table clear error. Either does not exist or other. \n{ValueError}")
+			connect.commit()
+			connect.close()
+			pass
+
+
+
+# dbh = DBHandler()
+# table_name = 'tech_projects_test_3'
 # test_title = 'Building a Flask Web App from Scratch'
 # test_body = "To start off the new year in 2022 and my journey into tech, I've decided to begin by creating a portfolio site for me to store all my projects and talk about them, the challanges I faced creating them, and the technologies I explored using them. So to start off, I wanted to make this site almost completely from scratch. So after some research, I landed on using the Python micro framework, Flask, as the backend server for this site, as it is light weight, quick to start, and easy to maintain. Using Flask allows for HTML and CSS to be set up on a Heroku Deployment quickly and with little maintaince. I..."
 # test_img_loc = 'static/images/thumbs/website_thumb.png'
-# # dbh.add_post(test_title, test_body, test_img_loc)
-dbh.inspect_table(table_name)
-# # dbh.del_post(2, table_name)
+# test_repo_link = "https://github.com/Mroussell/mjr_website"
+# dbh.add_post(table_name,test_title, test_body, test_img_loc, test_repo_link)
+# # dbh.create_table(table_name)
+# dbh.inspect_table(table_name)
+# # dbh.del_post(1, table_name)
 
--- post_creator.py
+++ post_creator.py
@@ -13,8 +13,17 @@ MIT Education License Preferred.
 import os
 import json
 import glob
+import sys
 from dotenv import load_dotenv
 from dbhandler import DBHandler
+
+
+# Check for empty directory
+dir = os.listdir(".new_posts")
+if len(dir) <= 0:
+	print("No files to add. Please add new post in correct format to the .new_posts directory. \nExiting!")
+	quit()
+
 
 # Use all text files in new post directory to create new post.
 os.chdir(".new_posts")
@@ -24,11 +33,25 @@ for file in glob.glob("*.txt"):
 		file_json = json.load(tf)
 		post_json_files.append(file_json)
 
+# Post are added in proper json form
 dbh = DBHandler()
 for post in post_json_files:
 	title = post['title']
 	body = post['body']
 	img_loc = post['post_img_loc']
-	dbh.add_post(title, body, img_loc)
+	repo_link = post['repo_link']
+	dbh.add_post('tech_projects_test_3',title, body, img_loc, repo_link)
 	print(f"Added post with title: {title}")
 
+# If added argument for clear directory
+if len(sys.argv) > 1 and sys.argv[1] == '-c':
+	print("Clearing posts from local directory: ~~~~~")
+	# Delete added posts from local directory.
+	for file in glob.glob("*.txt"):
+		os.remove(file)
+		print(f"Removed post file: {file}")
+else: 
+	print("Post NOT cleared from local directory. WARNING!!! Please Clear Manually.")
+
+
+print("Added post to database. Exit 0.")
